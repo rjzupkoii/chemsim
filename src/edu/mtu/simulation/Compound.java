f933ee28fab2c85878ec2ee72fe05ab39276799f
rjzupkoii <rjzii@yahoo.com>
2017-01-30 00:07:17 -0500

    Managed to get Java3D working with the simulation.

---
+++ src/edu/mtu/simulation/Compound.java
@@ -0,0 +1,22 @@
+package edu.mtu.simulation;
+
+import sim.engine.SimState;
+import sim.engine.Steppable;
+
+/**
+ * The compound is the base class for all of chemical entities that we are simulating.
+ */
+@SuppressWarnings("serial")
+public class Compound implements Steppable {
+
+	@Override
+	public void step(SimState state) {
+		// Move the compound in space
+		
+		// Check to see if the compound has bumped into another one
+		
+		// Perform any interactions between the compounds
+		
+	}
+
+}
