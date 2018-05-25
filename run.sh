9fbf12a982dac55ad4316a43d8b0066a637f4a87
rjzupkoii <rjzii@yahoo.com>
2018-05-24 23:48:42 -0400

    Updated ignore file, updated run script.

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Check for expected parameter
+if [ -z "$1" ]; then
+  echo "Usage: ./run.sh [pathway]"
+  exit 1
+fi
+REACTIONS=$1
+
 # Note the inputs
 experiment=experiment/experiment.csv
 chemicals=experiment/chemicals.csv
-reactions=experiment/reactions.csv
+reactions=experiment/$REACTIONS.csv
 path=../data
-zip=../scavenging.zip
+zip=../$REACTIONS.zip
 
 # Clean-up the data directory
 rm -rf data
@@ -14,7 +21,7 @@ mkdir data
 # Run the simulation
 for ndx in {1..10}
 do
-  java -javaagent:lib/SizeOf.jar -jar ChemSim.jar -n $ndx -r $reactions -c $chemicals -e $experiment 
+  java -javaagent:lib/SizeOf.jar -jar ChemSim.jar -n $ndx -r $reactions -c $chemicals -e $experiment -l 1000000
 done
 
 # Move the results, last console
