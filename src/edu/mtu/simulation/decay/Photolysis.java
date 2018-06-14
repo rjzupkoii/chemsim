a4bf5435c635de52356299ca4f46676a62401b20
rjzupkoii <rjzii@yahoo.com>
2018-06-14 19:02:56 -0400

    Updated photolysis to mM/L inputs

--- src/edu/mtu/simulation/decay/Photolysis.java
+++ src/edu/mtu/simulation/decay/Photolysis.java
@@ -27,6 +27,11 @@ public class Photolysis implements DecayModel {
 		return quantity;
 	}
 
+	/**
+	 * Prepare for photolysis on the basis of the slope provided. Note that we
+	 * are assuming that the slope is being given as mM/L and will need to be
+	 * adjusted for the reactor.
+	 */
 	@Override
 	public void prepare(String fileName) throws IOException {
 		// TODO Adjust this for multiple compounds
@@ -36,12 +41,19 @@ public class Photolysis implements DecayModel {
 		long count = ChemSim.getTracker().getCount("H2O2");
 		double rate = Parser.parseRate(fileName);
 		double volume = Parser.parseVolume(fileName);
+
+		// Note the scaling factor for mols to molecules
+		double scaling = ChemSim.getProperties().getMoleculeToMol();
+		
+		// Prepare the adjustment for mM/L to mols/reactor
+		double adjustment = volume * 0.001;
 		
 		// Hydrogen peroxide is a linear decay, or f(x) = C - r * t 
 		// this means we need to determine the odds that any individual 
 		// hydrogen peroxide agent will be removed each time step based upon
 		// the new population which requires us knowing the initial decay
-		quantity = (int)Math.ceil(Math.abs((count * rate * volume) / chemical.mols)) * ChemSim.SCALING;
+		quantity = (int)Math.ceil(Math.abs(rate * adjustment * scaling));
+		
 		System.out.println("H2O2 photolysis decay rate: " + quantity + " moleclules/timestep");
 		
 		// Since we know the decay rate we can calculate the running time
